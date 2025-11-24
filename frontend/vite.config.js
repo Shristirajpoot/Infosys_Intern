2683cf0d1983f0a53f3f3a37850cceb099ef95c1
👩‍💻Shristi Rajpoot <147296860+Shristirajpoot@users.noreply.github.com>
2025-11-24 09:38:21 +0530

    Add Vite configuration for React application

---
+++ frontend/vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [
+    react(),
+  ],
+  css: {
+    postcss: './postcss.config.js',
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
+})
